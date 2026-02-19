1fdfdfe8b5f4556ce727386fe0cf8635e59426a0
unknown <342896818@TDSB.ON.CA>
2026-02-19 10:06:44 -0500

    Added insignificant comment

--- assignment1/DiceRoller.java
+++ assignment1/DiceRoller.java
@@ -53,7 +53,7 @@ public class DiceRoller {
                             break;
                         }
 
-                    break;
+                    break; // Break
                 }
 
             }
